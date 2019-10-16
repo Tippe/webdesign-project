7d96f0449f7282d2a35dbf2eaedf5922683d8203
pieter bakker <18117805@student.hhs.nl>
2019-10-16 13:48:25 +0200

    css invoegen

--- contact.php
+++ contact.php
@@ -33,10 +33,10 @@
 			<!-- Card 2 -->
 			<div class="card mb-3">
 				<div class="row no-gutters">
-					<div class="col-md-4">
-						<img src="img/pasfoto_pieter.jpg" class="card-img" alt="...">
+					<div class="col-md-3">
+						<img src="img/pasfoto_pieter.jpg" class="contact-img card-img" alt="...">
 					</div>
-					<div class="col-md-8">
+					<div class="col-md-9">
 						<div class="card-body">
 							<h5 class="card-title">Pieter Bakker</h5>
 							<p class="card-text">Studentnummer: 2160778</p>
