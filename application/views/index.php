c127a4a90fe5a8b1df74aa4a4e9cfb1fdfa39b57
tippe_99 <tippegast@gmail.com>
2019-10-16 11:18:29 +0200

    test commit

--- application/views/index.php
+++ application/views/index.php
@@ -14,7 +14,7 @@
 
 			<div class="collapse" id="collapseExample">
 				<div class="card card-body">
-					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+					Hello World!
 				</div>
 			</div>
 		</div>
