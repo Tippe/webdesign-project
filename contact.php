2138d91c0396b1a850cca108b3fa823d2b4cebe0
tippe_99 <tippegast@gmail.com>
2019-10-16 13:10:59 +0200

    added contactinformation to the contact page

--- contact.php
+++ contact.php
@@ -1,1 +1,53 @@
-<p>Aahw man</p>
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Contact</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12">
+			<!-- Card 1 -->
+			<div class="card mb-3">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="..." class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h5 class="card-title">Tippe van Roosmalen</h5>
+							<p class="card-text">Studentnummer: 2153265</p>
+							<p class="card-text">E-mail: tr.vanroosmalen@student.avans.nl</p>
+							<p class="card-text">Klas: 23INVT1C</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<!-- Card 2 -->
+			<div class="card mb-3">
+				<div class="row no-gutters">
+					<div class="col-md-4">
+						<img src="..." class="card-img" alt="...">
+					</div>
+					<div class="col-md-8">
+						<div class="card-body">
+							<h5 class="card-title">Pieter Bakker</h5>
+							<p class="card-text">Studentnummer: </p>
+							<p class="card-text">E-mail: @student.avans.nl</p>
+							<p class="card-text">Klas: 23INVT1C</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include "includes/footer.php"; ?>
