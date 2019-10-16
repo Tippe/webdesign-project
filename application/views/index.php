f7c26da096dd95eaad28ca8efe70706976b9bdd7
tippe_99 <tippegast@gmail.com>
2019-10-16 11:14:49 +0200

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,25 @@
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-12">
+			<h1>Homepagina</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Content</button>
+
+			<div class="collapse" id="collapseExample">
+				<div class="card card-body">
+					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
