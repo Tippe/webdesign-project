185be1025b4aa64623e0f922bc4b62e24f902282
tippe_99 <tippegast@gmail.com>
2019-10-20 04:22:47 +0200

    New pages for SMART study methodes and personal info. Filled Tippe page

---
+++ pieter.php
@@ -0,0 +1,13 @@
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="display-3">Pieter Bakker</h1>
+		</div>
+	</div>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
---
+++ tippe.php
@@ -0,0 +1,106 @@
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="display-3">Tippe van Roosmalen</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<h5>Valkuilend studiemethode:</h5>
+			<p>Ik heb erg moeite met het plannen van dingen. Ik heb meestal een vaag idee wanneer ik ergens aan ga werken en plan daardoor erg weinig. Als ik iets plan dan is dat vaak een dag of een paar dagen van te voren.</p>
+			<h5>S.M.A.R.T.</h5>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-9">
+			<p>
+				<a class="btn btn-primary" data-toggle="collapse" href="#specifiek" role="button" aria-expanded="false" aria-controls="specifiek">Specifiek</a>
+			</p>
+
+			<div class="row pb-3">
+				<div class="col-md-12">
+					<div class="collapse" id="specifiek">
+						<div class="card card-body">
+							<h5 class="card-title">Specifiek</h5>
+							Ik ga beter plannen hoe ik mijn huiswerk en leerwerk indeel per week. Daarmee ben ik van plan aan het begin van de week mijn dagen in te vullen voor de hele week en daarbij zetten wat ik welke dag ga doen. En als er huiswerk is midden in de week dat dezelfde week nog af moet vul ik de week hier op aan en pas ik mogelijk al eerder geplande taken aan.
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<p>
+				<a class="btn btn-primary" data-toggle="collapse" href="#meetbaar" role="button" aria-expanded="false" aria-controls="meetbaar">Meetbaar</a>
+			</p>
+
+			<div class="row pb-3">
+				<div class="col-md-12">
+					<div class="collapse" id="Meetbaar">
+						<div class="card card-body">
+							<h5 class="card-title">Meetbaar</h5>
+							Ik ga in mijn agenda plannen opschrijven per dag en dat nakomen in de praktijk
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<p>
+				<a class="btn btn-primary" data-toggle="collapse" href="#acceptabel" role="button" aria-expanded="false" aria-controls="acceptabel">Acceptabel</a>
+			</p>
+
+			<div class="row pb-3">
+				<div class="col-md-12">
+					<div class="collapse" id="acceptabel">
+						<div class="card card-body">
+							<h5 class="card-title">Acceptabel</h5>
+							Ik accepteer dit doel en vind dat het erg goed kan helpen om beter te studeren. Hierdoor verwijder ik stress dat mogelijk gecreëerd kan worden door niks te plannen en moeten haasten aan het einde van een deadline.
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<p>
+				<a class="btn btn-primary" data-toggle="collapse" href="#realitsich" role="button" aria-expanded="false" aria-controls="realitsich">Realistisch</a>
+			</p>
+
+			<div class="row pb-3">
+				<div class="col-md-12">
+					<div class="collapse" id="realitsich">
+						<div class="card card-body">
+							<h5 class="card-title">Realistisch</h5>
+							Dit doel is zeer zeker te halen. Ik ben er goed toe in staat het doel te halen.
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<p>
+				<a class="btn btn-primary" data-toggle="collapse" href="#tijdsgebonden" role="button" aria-expanded="false" aria-controls="tijdsgebonden">Tijdsgebonden</a>
+			</p>
+
+			<div class="row pb-3">
+				<div class="col-md-12">
+					<div class="collapse" id="tijdsgebonden">
+						<div class="card card-body">
+							<h5 class="card-title">Tijdsgebonden</h5>
+							Ik zou aan het einde van (elke) week kunnen terugkijken of ik het doel heb gehaald om alle taken af te krijgen en/of aan te werken wanneer gepland. Maar specifiek voor deze opdracht is het tijdsgebonden aan 20 oktober 2019.
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="col-md-3">
+			<img class="img-fluid" src="img/tippe.jpg" />
+
+		</div>
+	</div>
+
+</div>
+
+<?php include "includes/footer.php"; ?>
