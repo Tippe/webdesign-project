360175a55228c68a8cb93b5d49b47334c9f845ad
Pieter Bakker <18117805@student.hhs.nl>
2020-01-15 21:39:07 +0100

    css toevoeging

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include "includes/header.php"; ?>
 
+
+<button class="btn btn-darkmode" onclick="myFunction()"> Dark mode aan/uit </button>
+
+
 <div class="container">
 
 	<div class="row">
@@ -9,8 +13,10 @@
 	</div>
 
 	<div class="row">
-		<div class="col-md-8 col-sm-12">
-			<p>Tippe van Roosmalen en ik (Pieter Bakker) zijn studenten van de opleiding Informatica bij Avans Breda. In deze tekst willen wij aan u, de lezer, duidelijk maken hoe deze opleiding eruit ziet wat betreft inhoud en het leertraject. Om te beginnen is het een 4 jarige studie die, in dit geval, gevolgd word bij Avans in Breda. Deze 4 jaren zijn als volgt ingedeeld waarbij we dieper gaan kijken naar het eerste jaar.</p>
+		<div class="col-md-7 col-sm-12">
+			<p>Tippe van Roosmalen en ik (Pieter Bakker) zijn studenten van de opleiding Informatica bij Avans Breda. In deze tekst willen wij aan u, de lezer, 
+				duidelijk maken hoe deze opleiding eruit ziet wat betreft inhoud en het leertraject. Om te beginnen is het een 4 jarige studie die, in dit geval, 
+				gevolgd word bij Avans in Breda. Deze 4 jaren zijn als volgt ingedeeld waarbij we dieper gaan kijken naar het eerste jaar.</p>
 		</div>
 		<div class="col-md-4 col-sm-12">
 			<img class="img-fluid" src="img/Avans_Hogeschool_Logo.png" alt="https://nl.wikipedia.org/wiki/Bestand:Avans_Hogeschool_Logo.svg" />
@@ -19,18 +25,34 @@
 
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
-			<p>Gedurende het eerste, en ons huidige, schooljaar word je bekend gemaakt met de verschillende onderwerpen binnen informatica zoals: programmeren, web design, databases en bedrijfsprocessen. Hierbij word je de basis geleerd zodat je “eenvoudige” code kan schrijven en websites kunt bouwen. De manier van les bestaat vooral uit uitleg krijgen waarna je zelfstandig opdrachten en oefeningen maakt om deze kennis toe te passen en te begrijpen. Daarnaast komen er een aantal groepsopdrachten/projecten langs waardoor je leert om in groepsverband te werken en hoe je scrum moet gebruiken.</p>
+			<p>Gedurende het eerste, en ons huidige, schooljaar word je bekend gemaakt met de verschillende onderwerpen binnen informatica zoals: programmeren, web design, databases 
+				en bedrijfsprocessen. Hierbij word je de basis geleerd zodat je “eenvoudige” code kan schrijven en websites kunt bouwen. De lesvorm bestaat vooral uit uitleg krijgen
+				waarna je zelfstandig opdrachten en oefeningen maakt om deze kennis toe te passen en te begrijpen. Daarnaast komen er een aantal groepsopdrachten/projecten langs 
+				waardoor je leert om in groepsverband te werken en hoe je scrum moet gebruiken.</p>
 
-			<p>Als we wat gedetailleerder kijken naar de start van de opleiding kunnen we zeggen dat, hoewel je de eerste week al gewoon les hebt, het tempo niet te hoog ligt en het niveau ook niet. Je begint namelijk echt bij de basis van de vakken en hierbij word duidelijk uitgelegd wat de functie is van elk relevant element zodat je ook echt weet waarvoor staat.</p>
-			<p>Het tempo is zoals eerder vermeld niet te hoog maar wel zodanig dat een achterstand van 2 weken weg werken moeilijk is en de moeilijkheidsgraad gaat ook met elke week stevig omhoog. In andere worden kan je dus zeggen dat ervaring niet nodig is in de opleiding maar het helpt uiteraard wel als je al het een en ander weet, zeker als je projecten moet doen en de stof moeilijk word.Wat betreft de vakken vanaf week 1, deze kun je zien op de website van Avans. hier is een duidelijk overzicht van de periodes en de vakken die hierin behandeld worden.</p>
+			<p>Als we wat gedetailleerder kijken naar de start van de opleiding kunnen we zeggen dat, hoewel je de eerste week al gewoon les hebt, het tempo niet te hoog ligt en het niveau ook niet.
+				Je begint namelijk echt bij de basis van de vakken en hierbij word duidelijk uitgelegd wat de functie is van elk relevant element zodat je ook echt weet waarvoor het staat.</p>
+
+			<p>Het tempo is zoals eerder vermeld niet te hoog maar wel zodanig dat een achterstand van 2 weken weg werken moeilijk is en de moeilijkheidsgraad gaat ook met elke week
+				stevig omhoog. In andere worden kan je dus zeggen dat ervaring niet nodig is in de opleiding maar het helpt uiteraard wel als je al het een en ander weet, 
+				zeker als je projecten moet doen en de stof moeilijk word.Wat betreft de vakken vanaf week 1, deze kun je zien op de website van Avans. hier is een duidelijk overzicht 
+				van de periodes en de vakken die hierin behandeld worden.</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12 col-sm-12">
-			<p> Het tweede jaar bestaat, net als jaar 1, uit lessen en geen stage. Je krijgt hierbij nieuwe vakken waarin je naar complexe zaken gaat kijken zoals machine learning en algoritmes.<br />Jaar 3 en 4 bestaan o.a. uit stage  waar jaar 3 redelijk 50/50 is verdeeld en jaar 4 juist voor het grootste deel bestaat uit een stage, namelijk de afstudeer stage.</p>
+			<p> Het tweede jaar bestaat, net als jaar 1, uit lessen en geen stage. Je krijgt hierbij nieuwe vakken waarin je naar complexe zaken gaat kijken zoals machine learning en algoritmes.
+				<br/>Jaar 3 en 4 bestaan o.a. uit stage  waar jaar 3 redelijk 50/50 is verdeeld en jaar 4 juist voor het grootste deel bestaat uit een stage, namelijk de afstudeer stage.</p>
 		</div>
 	</div>
-</div>
+
+	<div class="row">
+		<div class="col-md-12 col-sm-12">
+			<p class="fat"> klik <a href="https://www.avans.nl/"><span class="highlight"> hier </span></a> om naar de Avans website te gaan </p> 
+		</div>
+	</div>
+
+<script src="Javascript/index.js" type="text/javascript"></script>
 
 <?php include "includes/footer.php"; ?>
