be7ddeb05e84ea664ae5e80a79e94213643e56c8
tippe_99 <tippegast@gmail.com>
2020-01-19 21:47:47 +0100

    Fixed footer responsiveness

--- includes/footer.php
+++ includes/footer.php
@@ -1,17 +1,20 @@
 		<!-- End of the page-specific content. -->
-		<footer class="footer" style="  position: absolute; bottom: 0; width: 100%; height: 60px; background-color: #f5f5f5;">
+		<footer class="footer">
 			<div class="container">
 				<p class="text-muted">
-					<button class="btn btn-darkmode" onclick="myFunction()">Dark mode aan/uit</button>
+					<button class="btn btn-darkmode" onclick="turnDark();">Dark mode aan</button>
+					<button class="btn btn-darkmode" onclick="turnLight();">Dark mode uit</button>
 					Deze website is gemaakt door: Tippe van Roosmalen & Pieter Bakker ~ <i><?= date('Y'); ?></i>
 				</p>
 			</div>
 	    </footer>
-
 	</body>
 
 	<!-- Bootstrap jQuery & Javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+	<!-- Custom Javascript -->
+	<script src="javascript/script.js"></script>
 
 </html>
